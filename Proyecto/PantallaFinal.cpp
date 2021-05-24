6e79f3c5afb3f343630f1d0ba7bde063193a9b2b
Kevin-Santillan <80121706+Kevin-Santillan@users.noreply.github.com>
2021-05-23 22:20:54 -0500

    Update PantallaFinal.cpp

--- Proyecto/PantallaFinal.cpp
+++ Proyecto/PantallaFinal.cpp
@@ -6,17 +6,13 @@
 
 using namespace std;
 PantallaFinal::PantallaFinal() {
-
 }
 
 void PantallaFinal::dibujar() {
-
 }
 
 void PantallaFinal::eventos() {
-
 }
 
 void PantallaFinal::actualizar_mensaje() {
-
 }
